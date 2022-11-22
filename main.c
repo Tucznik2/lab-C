46e596917cac2335c5cf6bbdcd6a35238cf96a53
Filip Kula <f3kula@gmail.com>
2022-11-23 00:23:28 +0100

    third meeting version

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 
-void main() {
-    int indeks = 173654, rokStudiow = 1, wybranaOpcja, opcje[] = {-1, 1, 2, 3}, isPresent;
+int countOddNumber() {
+    int liczba, count = 0;
     do {
-        isPresent = 0;
+        printf("%s", "Wprowadz liczbe: ");
+        scanf("%d", &liczba);
+        if (liczba % 2 != 0) {
+            count += 1;
+        }
+    } while (liczba != 0);
+    return count;
+}
+
+void secondOption(int wybranaOpcja);
+
+void thirdOption(int wybranaOpcja);
+
+void defaultOption() {
+    printf("Wybrano opcje z poza zakresu\n");
+}
+
+int main_menu(int indeks, int rokStudiow) {
+    int wybranaOpcja;
+    do {
         printf("Program w jezyku C wykonany przez studenta o indeksie nr %d na %d roku studiow\n", indeks, rokStudiow);
         printf("Menu:\n- opcja -1:\n- opcja 1:\n- opcja 2:\n- opcja 3:\nWybierz opcje:");
         scanf("%d", &wybranaOpcja);
@@ -11,16 +30,22 @@ void main() {
             case -1:
                 break;
             case 1:
-                printf("Wybrano opcje: %d\n", wybranaOpcja);
+                printf("Uzytkownik wpisal %d liczb nieparzystych\n", countOddNumber());
                 break;
             case 2:
-                printf("Wybrano opcje: %d\n", wybranaOpcja);
+                secondOption(wybranaOpcja);
                 break;
             case 3:
-                printf("Wybrano opcje: %d\n", wybranaOpcja);
+                thirdOption(wybranaOpcja);
             default:
-                printf("Wybrano opcje z poza zakresu\n");
+                defaultOption();
         }
     } while (wybranaOpcja != -1);
     printf("Do widzenia!");
+}
+
+void main() {
+    int indeks = 173654, rokStudiow = 1, wybranaOpcja;
+    main_menu(indeks, rokStudiow);
+
 }
